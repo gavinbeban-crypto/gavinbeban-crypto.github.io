45cd38a66a25b6ecf6ee291ced77014b02865b06
gavinwekek <gavinbeban@gmail.com>
2026-02-08 04:57:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const animatedElements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.2 });
+
+animatedElements.forEach(el => observer.observe(el));
